858376d511a970f57cf51b1226a1ed365552a8c8
Ambili das v s <ambili.kan@gmail.com>
2022-06-26 20:32:46 +0530

    Ability to add multiple employee to payroll

---
+++ EmployeePayrollOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRollThreading
+{
+    public class EmployeePayrollOperations
+    {
+        List<EmployeeDetails> employeePayrollDetailList= new List<EmployeeDetails>();
+        //UC1
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeePayroll(employeeData);
+                Console.WriteLine("Employee added: " + employeeData.EmployeeName);
+
+            });
+            Console.WriteLine(this.employeePayrollDetailList.ToString());
+        }
+        public void addEmployeePayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailList.Add(emp); 
+        }
+    }
+}
